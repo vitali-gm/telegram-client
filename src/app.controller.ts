1feca867b1ed022b0c9ea0181f55f2aee0cd6f32
vitalik <kovalchuk.vitalik42@gmail.com>
2021-12-14 17:53:57 +0200

    test tglib

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { MessagePattern } from '@nestjs/microservices';
+// import { Client } from 'tglib';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @MessagePattern({ cmd: 'test' })
+  @Get()
   getHello(): string {
-    console.log('test message');
+    // const client = new Client({
+    //   apiId: '9075328',
+    //   apiHash: 'a904d865881db19aa0f242929d455e28',
+    //   verbosityLevel: 2,
+    // });
+    //
+    // client.ready;
+    //
+    // console.log(client);
     return this.appService.getHello();
   }
 }
