7a1511f932d27d74f40e97e6a1901ae9988da247
Vitaliy Kovalchuk <kovalchuk.vitalik42@gmail.com>
2022-01-31 10:19:52 +0200

    update reread message id

--- src/app.service.ts
+++ src/app.service.ts
@@ -129,7 +129,7 @@ export class AppService {
             '@type': 'searchChatMessages',
             chat_id: parseInt(-100 + String(item.chatId)), //-1001489609347,
             // chat_id: -1001043205127,
-            query: item.origin.message,
+            query: item.origin.message.substring(0, 100),
             limit: 20
           });
 
